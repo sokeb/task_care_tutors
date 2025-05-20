ab1257866433fd64da58fcde5a39efd1cb4da53b
mksakib126 <mksakib126@gmail.com>
2025-05-20 11:07:23 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/task_care_tutors/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.task_care_tutors
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/pages/home_page.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.amber,
+        ),
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size.fromHeight(50),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            backgroundColor: Colors.amber,
+            foregroundColor: Colors.black,
+            textStyle: const TextStyle(fontSize: 18),
+          ),
+        ),
+      ),
+      home: HomePage(),
+    );
+  }
+}
